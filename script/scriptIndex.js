e5070f618aca9e75c439cd3cd9cdbb600e3bbc76
ExcitedSaber <yuan.bowen@yahoo.com>
2021-03-04 21:57:13 +1100

    try to build a clock

---
+++ script/scriptIndex.js
@@ -0,0 +1,8 @@
+function time() {
+    var date = new Date();
+    var date1 = date.toLocaleString();
+    var p1 = document.getElementById("date");
+    p1.innerHTML = date1;
+}
+
+setInterval("time()", 1000);
